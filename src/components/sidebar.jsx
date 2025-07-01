b4b7f06f6a23cc22fe827968d84f466469f8e1f6
unknown <pelin.d3mir@gmail.com>
2025-07-01 10:28:48 +0300

    İlk yükleme

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -2,7 +2,6 @@ function Sidebar() {
     return (
         <div className="sidebar">
             <h2 className="logo">CallPilot</h2>
-
             <input type="text" className="search-input" placeholder="Search..." />
 
             <div className="menu-section">
@@ -13,34 +12,36 @@ function Sidebar() {
                 <p className="menu-title">Konuşmalar</p>
                 <button className="menu-button">Bütün Konuşmalar</button>
                 <button className="menu-button">Favoriler</button>
+                <button className="menu-button">Özel Konuşmalar</button>
             </div>
 
             <div className="menu-section">
-                <p className="menu-title">Folders</p>
-                <p className="menu-item">Priority Conversations</p>
+                <hr className="sidebar-divider" />
+                <p className="menu-title">Dosyalar</p>
+                <p className="menu-item">Müşteri Bilgileri</p>
+            </div>
+            <hr className="sidebar-divider" />
+            <div className="menu-section">
+                <p className="menu-title">Takımlar</p>
+                <p className="menu-item">Admin</p>
+                <hr className="sidebar-divider" />
             </div>
 
             <div className="menu-section">
-                <p className="menu-title">Teams</p>
-
-            </div>
-
-            <div className="menu-section">
-                <p className="menu-title">Channels</p>
+                <p className="menu-title">Kanallar</p>
                 <ul className="channel-list">
-
-                    <li>PaperLayer Email</li>
-
+                    <li>Email</li>
+                    <li>Facebook</li>
                 </ul>
+                <hr className="sidebar-divider" />
             </div>
 
-            <div className="user-info">
-                <p>Azra</p>
-                <span>ıknowtechnology@gmail.com</span>
+            <div className="user-info-box">
+                <span className="user-name">Azra</span>
+                <span className="user-mail">ıknowtechnology@mail.com</span>
             </div>
         </div>
     );
 }
-
 
 export default Sidebar;
